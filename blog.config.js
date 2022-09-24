1347229b5c3928509c93bbcb41943a28d1929128
ruiqieth <114266862+ruiqieth@users.noreply.github.com>
2022-09-24 08:54:33 +0800

    Update blog.config.js

--- blog.config.js
+++ blog.config.js
@@ -3,7 +3,7 @@ const BLOG = {
   author: 'Ruiqi',
   email: 'Ruiqi@duck.com',
   link: 'https://ruiq.vercel.app',
-  description: 'This gonna be an awesome website.',
+  description: '我的web3冲浪记录.',
   lang: 'zh-CN', // ['en-US', 'zh-CN', 'zh-HK', 'zh-TW', 'ja-JP', 'es-ES']
   appearance: 'auto', // ['light', 'dark', 'auto'],
   font: 'sans-serif', // ['sans-serif', 'serif']
@@ -17,9 +17,9 @@ const BLOG = {
   showArchive: true,
   autoCollapsedNavBar: false, // The automatically collapsed navigation bar
   ogImageGenerateURL: 'https://og-image-craigary.vercel.app', // The link to generate OG image, don't end with a slash
-  socialLink: 'https://twitter.com/craigaryhart',
+  socialLink: 'https://twitter.com/ruiqieth',
   seo: {
-    keywords: ['Blog', 'Website', 'Ruiqi'],
+    keywords: ['Blog', 'Website', 'Ruiqieth'],
     googleSiteVerification: '' // Remove the value or replace it with your own google site verification code
   },
   notionPageId: process.env.NOTION_PAGE_ID, // DO NOT CHANGE THIS！！！
